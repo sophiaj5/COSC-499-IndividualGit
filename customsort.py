872cfea8a7e4e07bbd77698673f6bd0d00479876
sophiaj5 <59673737+sophiaj5@users.noreply.github.com>
2021-09-16 01:17:15 -0800

    added and updated comments

--- customsort.py
+++ customsort.py
@@ -1,5 +1,6 @@
+#this program will sort the numbers in the given list
 def customSort(numArray):
-    length = len(numArray)
+    length = len(numArray) #getting the length of the list
     for i in range(length):
         for j in range(length - i - 1):
             if numArray[j] > numArray[j + 1]:
