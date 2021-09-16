c57d096ce04c7a030cfb7fe0ad8c7727ca9685ae
sophiaj5 <59673737+sophiaj5@users.noreply.github.com>
2021-09-16 00:31:58 -0800

    function feature added

--- minimum.py
+++ minimum.py
@@ -1,4 +1,11 @@
 #this is a program to find the smallest number in a list
 
+def findMinNum(numArray):
+    if type(numArray) == list:
+        numArray.sort()
+        return numArray[0]
+    else:
+        raise ValueError("Was expecting a list")
+
 numbers = [13, 7, 16, 28, 2]
-print(min(numbers))
+print("This is the smallest number:", findMinNum(numbers))
